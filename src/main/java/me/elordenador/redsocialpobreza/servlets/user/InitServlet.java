caeee9d92be92b231b3f85d0bf75a3a99ddb5dc3
Daniel Sánchez (elordenador) <54474838+dsaub@users.noreply.github.com>
2025-04-09 17:59:55 +0200

    Update InitServlet.java

--- src/main/java/me/elordenador/redsocialpobreza/servlets/user/InitServlet.java
+++ src/main/java/me/elordenador/redsocialpobreza/servlets/user/InitServlet.java
@@ -3,6 +3,7 @@ package me.elordenador.redsocialpobreza.servlets.user;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import me.elordenador.redsocialpobreza.DBManager;
+import com.github.lalyos.jfiglet.FigletFont;
 
 @WebServlet(name = "InitServlet", value="/", loadOnStartup=1)
 public class InitServlet {
