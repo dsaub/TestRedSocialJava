0acad9558a4482179d6a63f98a24d1ea6ba2adaf
Daniel <danilacasito8@gmail.com>
2025-04-09 11:54:36 +0200

    add GetTokenServlet for token retrieval API

---
+++ src/main/java/me/elordenador/redsocialpobreza/servlets/user/GetTokenServlet.java
@@ -0,0 +1,9 @@
+package me.elordenador.redsocialpobreza.servlets.user;
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(name = "GetToken-Servlet", value="/api/v1/getToken")
+public class GetTokenServlet {
+
+
+}
