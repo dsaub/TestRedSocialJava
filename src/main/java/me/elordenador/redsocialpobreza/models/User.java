21b2f5d75e23ff398f3f732dac6be52d57ba55c2
Daniel <danilacasito8@gmail.com>
2025-04-11 08:48:16 +0200

    Add Guava dependency and implement password hashing in User model

--- src/main/java/me/elordenador/redsocialpobreza/models/User.java
+++ src/main/java/me/elordenador/redsocialpobreza/models/User.java
@@ -1,10 +1,13 @@
 package me.elordenador.redsocialpobreza.models;
 
 
+import com.google.common.hash.Hashing;
 import com.sun.istack.NotNull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import java.nio.charset.StandardCharsets;
 
 @Entity
 @Table(name = "users")
@@ -22,7 +25,16 @@ public class User {
 
     public User(String username, String password) {
         this.username = username;
-        this.password = password;
+        this.password = Hashing.sha256().hashString(password, StandardCharsets.UTF_8).toString();
+    }
+
+    public boolean setPassword(String originalPassword, String newPassword) {
+        String hashedOriginalPassword = Hashing.sha256().hashString(originalPassword, StandardCharsets.UTF_8).toString();
+        String hashedNewPassword = Hashing.sha256().hashString(newPassword, StandardCharsets.UTF_8).toString();
+        if (hashedOriginalPassword.equals(hashedNewPassword)) return false;
+        if (!hashedOriginalPassword.equals(password)) return false;
+        password = hashedNewPassword;
+        return true;
     }
 
 }
