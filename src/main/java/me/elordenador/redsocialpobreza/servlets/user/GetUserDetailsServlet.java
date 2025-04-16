599c869209989358cbc73f38f7ba177181f2b29d
daniel <danilacasito8@gmail.com>
2025-04-16 17:45:32 +0200

    Create a servlet for getting via post the data of a user.

---
+++ src/main/java/me/elordenador/redsocialpobreza/servlets/user/GetUserDetailsServlet.java
@@ -0,0 +1,43 @@
+package me.elordenador.redsocialpobreza.servlets.user;
+
+
+import com.google.gson.Gson;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import me.elordenador.redsocialpobreza.DBManager;
+import me.elordenador.redsocialpobreza.models.User;
+import me.elordenador.redsocialpobreza.outmodels.oUser;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "GetUserServlet", value = "/api/v1/getUser")
+public class GetUserDetailsServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        SessionFactory sessionFactory = DBManager.getSessionFactory();
+
+
+        sessionFactory.inTransaction(session -> {
+            Query<User> query = session.createQuery("from User where username = :username", User.class);
+            query.setParameter("username", username);
+            User user = query.uniqueResult();
+            Gson gson = new Gson();
+            oUser ouser = new oUser();
+            ouser.username = user.getUsername();
+            ouser.fullname = user.getFullName();
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            try {
+                PrintWriter out = response.getWriter();
+                out.println(gson.toJson(ouser));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}
